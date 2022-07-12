cf57b477b040dcf679fd45573386ee4dd3c9d68d
Lucas klafke <lucasnkz11@gmail.com>
2022-07-12 15:07:01 -0300

    ssl created in db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,14 +6,11 @@ dotenv.config();
 const { Pool } = pg;
 
 const connection = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
-// if (process.env.MODE === "PROD") {
-//   connection.ssl = {
-//     rejectUnauthorized: false
-//   };
-// }
 
 console.log("connected");
 export default connection;
